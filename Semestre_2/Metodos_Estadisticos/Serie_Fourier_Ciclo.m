cb680518edb87cdfa3e8f0fa8f2e6df0ad9fea35
Danglz16 <daniel.glz.avd@gmail.com>
2025-09-08 10:12:23 -0600

    ADD - Clase ME 20250908

---
+++ Semestre_2/Metodos_Estadisticos/Serie_Fourier_Ciclo.m
@@ -0,0 +1,24 @@
+clear all;
+close all;
+clc;
+w0=pi;
+T=(2*pi)/w0;
+t=-T/2:pi/300:T/2;
+cte=4/pi;
+term=200;
+n=1;
+%Armonico=(1/n)*sin(n*w0*t);
+Armonico = 0;
+plot(t,Armonico,'b','LineWidth',2);
+
+for n=1:2:term
+    Armonico=Armonico +((1/n)*sin(n*w0*t));
+    plot(t,Armonico,'b','LineWidth',2);
+    axis square;
+    grid on;
+    pause(0.3);
+end
+f_t=cte*Armonico;
+plot(t,f_t,'b','LineWidth',2);
+axis square
+grid on;
