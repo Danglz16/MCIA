911b128d9b8c36dfa07dfe5813c97dc167bdfa48
Danglz16 <daniel.glz.avd@gmail.com>
2026-02-16 19:19:45 -0600

    ADD - OC Clase Medir tiempos P1 - 20260216

--- Semestre_3/Optimizacion_Combinatoria/Programas/Measure Time/Measure_Time.cpp
+++ Semestre_3/Optimizacion_Combinatoria/Programas/Measure Time/Measure_Time.cpp
@@ -5,16 +5,16 @@
 using namespace std;
 int main(void)
 { //Program 2.31
-int a[100000], step = 10000;
-clock_t start, finish;
-long double t0,t1;
-cout << "CLOCKS PER SECOND: " << CLOCKS_PER_SEC<< endl;
-for (int n = step; n <= 100000; n += step) {
-for (int i = 0; i < n; i++) a[i] = n - i; // initialize
-start = clock( );
-InsertionSort(a, n);
-finish = clock( );
-t0= (finish - start); t1= t0/ CLOCKS_PER_SEC;
-cout << "# of cycles: " << t0 << "; Running-time: " << t1 << "secs." << endl << endl;
-}
+    int a[100000], step = 10000;
+    clock_t start, finish;
+    long double t0,t1;
+    cout << "CLOCKS PER SECOND: " << CLOCKS_PER_SEC<< endl;
+    for (int n = step; n <= 100000; n += step) {
+        for (int i = 0; i < n; i++) a[i] = n - i; // initialize
+        start = clock( );
+        InsertionSort(a, n);
+        finish = clock( );
+        t0= (finish - start); t1= t0/ CLOCKS_PER_SEC;
+        cout << "# of cycles: " << t0 << "; Running-time: " << t1 << "secs." << endl << endl;
+    }
 }
